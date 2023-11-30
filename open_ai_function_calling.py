ed785aa85475308a89554db489ce95818569856c
jjones <jimmy.jones@tutanota.com>
2023-11-29 17:48:04 -0700

    Add OpenAI function calling feature

--- open_ai_function_calling.py
+++ open_ai_function_calling.py
@@ -36,3 +36,28 @@ completetion = client.chat.completions.create(
 
 output = completetion.choices[0].message.content
 print(output)
+
+# ------------------------------------------------------------------
+# Use OpenAI's function calling feature
+# ------------------------------------------------------------------
+
+function_descriptions = [
+    {
+        "name": "get_flight_info",
+        "description": "Get flight information between two locations.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "loc_origin": {
+                    "type": "string",
+                    "description": "The departure airport, e.g. DUS",
+                },
+                "loc_destination": {
+                    "type": "string",
+                    "description": "The destination airport, e.g. HAM",
+                },
+            },
+            "required": ["loc_origin", "loc_destination"],
+        },
+    }
+]
