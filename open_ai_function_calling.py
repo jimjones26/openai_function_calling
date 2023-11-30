00aeea64b457cf4a9a29130546a5a3c76cdf523c
jjones <jimmy.jones@tutanota.com>
2023-11-29 17:32:05 -0700

    Add loading of OpenAI API key from .env file

--- open_ai_function_calling.py
+++ open_ai_function_calling.py
@@ -10,3 +10,10 @@ from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, AIMessage, ChatMessage
+
+# ------------------------------------------------------------------
+# Load opeaai api key from .env file
+# ------------------------------------------------------------------
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
